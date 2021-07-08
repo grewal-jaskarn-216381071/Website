d270b8984cd24cf02fc36c78e11fbf88aed91478
grewal-jaskarn-216381071 <46427824+grewal-jaskarn-216381071@users.noreply.github.com>
2021-07-07 21:22:17 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+function validation() {
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var subject = document.getElementById("subject").value;
+	var message = document.getElementById("message").value;
+	var error_message = document.getElementById("error_message");
+	var text;
+
+	error_message.style.padding = "10px";
+
+	if (name.length <4){
+		text = "Please Enter a Valid Name";
+		error_message.innerHTML = text;
+		return false;
+	}
+
+	if (email.indexOf("@") == -1 || email.indexOf(".") == -1 || email.length < 6){
+		text = "Please Enter a Valid Email";
+		error_message.innerHTML = text;
+		return false;
+	}
+
+	if (subject.length <5){
+		text = "Please Enter Correct Subject";
+		error_message.innerHTML = text;
+		return false;
+	}
+
+	if (message.length <= 8 ){
+		text = "Please Enter a Short Message";
+		error_message.innerHTML = text;
+		return false;
+	}
+
+	document.getElementById(name).value = "";
+	alert("Submitting Form")
+	
+	return true;
+
+}
